51cf01892ae7192018ba085bc84b54ca4d002f38
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-11 00:36:59 +0530

    added some endpoint

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 // const { log } = require('console');
-const express = require('express'); 
-const port = process.env.PORT || 9000;  // Use environment variable or default to 9000
-const db = require('./config/mongoose')
+const express = require("express");
+const port = process.env.PORT || 9000; // Use environment variable or default to 9000
+const db = require("./config/mongoose");
 const app = express();
 
 // middleware
 app.use(express.json());
 
 // const routesPath = require('./routes');
-app.use('/', require('./routes'));
+app.get("*", (req, res) => {
+  res.status(200).json({
+    message: "Successfully requested",
+    success: true,
+    error: false,
+  });
+});
+app.use("/", require("./routes"));
 
-// Server 
+// Server
 app.listen(port, (err) => {
-    if(err) {
-        console.log(`Error in running Server ${err}`);
-    }
-    console.log(`Server is running ${port}`);
+  if (err) {
+    console.log(`Error in running Server ${err}`);
+  }
+  console.log(`Server is running ${port}`);
 });
