44e8f5773439c42130df77bc636f0deca4350690
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-11 02:14:51 +0530

    added

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,17 @@
-const mongoos = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 // creating connection with database
-mongoos.connect('mongodb://127.0.0.1:27017/product', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log('Connected to the Database');
-}) 
-.catch ((err) => { 
+mongoose
+  .connect(process.env.DBHOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the Database");
+  })
+  .catch((err) => {
+    console.log(process.env.DBHOST);
     console.error(`Error in Conneting to the Database : ${err}`);
     process.exit(1); // Exit the Process if the database connection fails
-});
+  });
--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 // const { log } = require('console');
 const express = require("express");
-const port = process.env.PORT || 9000; // Use environment variable or default to 9000
+const productController = require("./controller/product_controller");
 const db = require("./config/mongoose");
 const routes = require("./routes/product");
 const app = express();
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
 
-const routesPath = require("./routes");
-// app.get("*", (req, res) => {
-//   res.status(200).json({
-//     message: "Successfully used",
-//     success: true,
-//     error: false,
-//   });
-// });
-// app.use("*", routes);
-// app.use("/", routes);
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "success fully get data",
-  });
-});
-app.post("/create", (req, res) => {
-  res.status(201).json({
-    message: "success fully created",
-  });
-});
+app.use("/", routes);
 
 // Server
+const port = process.env.PORT || 9000; // Use environment variable or default to 9000
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in running Server ${err}`);
