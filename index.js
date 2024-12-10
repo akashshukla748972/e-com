e904f7b42d07e8ea913d4cb73086e4363a761f35
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-11 01:25:24 +0530

    added

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(express.json());
 
 const routesPath = require("./routes");
-app.get("*", (req, res) => {
-  res.status(200).json({
-    message: "Successfully used",
-    success: true,
-    error: false,
-  });
-});
+// app.get("*", (req, res) => {
+//   res.status(200).json({
+//     message: "Successfully used",
+//     success: true,
+//     error: false,
+//   });
+// });
 // app.use("*", routes);
-// app.use("/", routes);
+app.use("/", routes);
 
 // Server
 app.listen(port, (err) => {
